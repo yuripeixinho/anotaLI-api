7e59863720bda3e3671c6bfd1838a8745f809b4c
Yago <peixinhoyago@gmail.com>
2024-10-22 21:33:25 -0300

    test: PerfilConta com inicializador

--- AL.Core/Domain/PerfilConta.cs
+++ AL.Core/Domain/PerfilConta.cs
@@ -1,11 +1,11 @@
 ﻿namespace AL.Core.Domain;
-public class PerfilConta
+public class PerfilConta(Conta conta)
 {
     public int PerfilContaID { get; set; }
     public required string Nome { get; set; }
 
     public required string ContaID { get; set; }
-    public required Conta Conta { get; set; }
+    public required Conta Conta { get; set; } = conta ?? throw new ArgumentNullException(nameof(conta));
 
     public List<Produto>? Produtos { get; set; }
 }
