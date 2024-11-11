64b10dff4cd7f46dac68793b6d52d3465c3caab3
Yuri <yuripeixinho03@gmail.com>
2024-11-10 21:32:20 -0300

    feat: imagens

---
+++ Core.Shared/ModelViews/Imagens/ImagemView.cs
@@ -0,0 +1,6 @@
+namespace AL.Core.Shared.ModelViews.Imagens;
+public class ImagemView
+{
+    public int Id { get; set; }
+    public string? CaminhoImagem { get; set; } // Caminho ou URL da imagem
+}
